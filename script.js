413c64d820b95ac4cc5e748df48c1f5ff1f1b868
amreinkr <143140217+amreinkr@users.noreply.github.com>
2023-09-12 14:32:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('hi!');
+
+function sleepIn(weekday, vacation){
+  let sleep = false;
+  if(!weekday || vacation){
+    sleep = true;
+    return sleep;
+  }
+  return sleep;
+}
+
+function monkeyTrouble(aSmile, bSmile){
+  let trouble = false;
+  if((aSmile && bSmile)||(!aSmile && !bSmile)){
+    trouble = true;
+    return trouble;
+  }
+  return trouble;
+}
+
+function sumDouble(a, b){
+  if(a == b){
+    return ((a + b)*2);
+  }
+  return a + b;
+}
+
